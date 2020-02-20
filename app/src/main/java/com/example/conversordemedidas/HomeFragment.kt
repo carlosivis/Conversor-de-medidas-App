68c69099f341994436d75009ea6ac4ed2f1d3ee7
Carlos Ivis <carlosivis1@gmail.com>
2020-02-20 11:54:10 -0300

    Navigation adicionada e modo temperatura iniciado

--- app/src/main/java/com/example/conversordemedidas/HomeFragment.kt
+++ app/src/main/java/com/example/conversordemedidas/HomeFragment.kt
@@ -1,16 +1,23 @@
 package com.example.conversordemedidas
 
 
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
+
+    var navController: NavController? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +27,21 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        view.findViewById<ImageView>(R.id.btnComp).setOnClickListener(this)
+        view.findViewById<ImageView>(R.id.btnPeso).setOnClickListener(this)
+        view.findViewById<ImageView>(R.id.btnTemp).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+    when(v!!.id){
+        R.id.btnComp->navController!!.navigate(R.id.action_homeFragment_to_compFragment)
+        R.id.btnPeso->navController!!.navigate(R.id.action_homeFragment_to_pesoFragment)
+        R.id.btnTemp->navController!!.navigate(R.id.action_homeFragment_to_tempFragment)
+    }
+    }
 
 }
--- app/src/main/java/com/example/conversordemedidas/TempFragment.kt
+++ app/src/main/java/com/example/conversordemedidas/TempFragment.kt
@@ -6,11 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass.
  */
 class TempFragment : Fragment() {
+
+    val unidades: Array<String> = arrayOf("Celsius", "Kelvin", "Fahrenheit")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +26,12 @@ class TempFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_temp, container, false)
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        val spinner:Spinner
+        val adapter = unidades
+
+    }
 
 }
